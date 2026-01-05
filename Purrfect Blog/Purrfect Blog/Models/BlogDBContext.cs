3801ab91fb3b5b1fbdc5717b88b3a830240308a9
mrdabek <81967634+mrdabek@users.noreply.github.com>
2026-01-05 18:47:58 +0100

    Add Entity Framework with BlogPost model

---
+++ Purrfect Blog/Purrfect Blog/Models/BlogDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Purrfect_Blog.Models
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+    }
+}
---
+++ Purrfect Blog/Purrfect Blog/Models/BlogPost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Purrfect_Blog.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
